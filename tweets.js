85c29e75184715c13cb436aa7f3da445b67d4140
jonas-kgomo <jonaskgmoo@gmail.com>
2020-06-05 22:25:00 +0300

    adding tweet functions

--- tweets.js
+++ tweets.js
@@ -4,44 +4,63 @@ var user = fs.readFileSync('./media/user.txt');
 var tweet = fs.readFileSync('./media/tweet.txt');
 
 
-// convert text to json 
-
-
-
 // follow 
 
-    var linef = user.toString().split('\r\n'); // line break, carriage return suitable on Windows
-
+function followParse(){
+    var lines = user.toString().split('\r\n'); // line break, carriage return suitable on Windows
     var follow = [];
 
-    for(var i = 0; i < linef.length; ++i){
+    for(var i = 0; i < lines.length; ++i){
 
-        var values = linef[i].split(' follows ');
+        var values = lines[i].split(' follows ');
         follow[i] = {user : values[0], follows : values[1].split(', ')};
-
-      
     }
+
+    return follow;
+}
     
 //console.log(follow);
 
 // tweet
 
+function tweetParse(){
 
+    var lines = tweet.toString().split('\r\n'); // line break suitable on Windows
+    var tweets = [];
 
-var lines = tweet.toString().split('\r\n'); // line break suitable on Windows
+    for(var i = 0; i < lines.length; ++i){
 
-var tweets = [];
+        var values = lines[i].split('> ');
+        tweets[i] = {user : values[0], tweet : values[1]};
 
-for(var i = 0; i < lines.length; ++i){
+        // truncate tweet if char > 280
+        if(tweets[i].tweet.length > 280)
+        console.log('your tweet is exeeding 280');
 
-    var values = lines[i].split('> ');
-    tweets[i] = {user : values[0], tweet : values[1]};
-    
-    // truncate tweet if char > 280
+       // console.log(tweets);
+    }
 
-    if(tweets[i].tweet.length > 280)
-    console.log('your tweet is exeeding 280');
-  
+    return tweets;
 }
 
-console.log(tweets);
+//console.log(tweetParse());
+var feed = followParse();
+var twt = tweetParse();
+
+function tweetFeed(){
+     
+      
+
+      seed = feed[0].user;
+      var flw = feed[0].follow;
+      twt = twt[0].tweet;
+
+      console.log(seed, twt);
+      console.log(`@flw`);    
+       
+  }
+  
+//console.log(tweetFeed())
+
+
+tweetFeed();
